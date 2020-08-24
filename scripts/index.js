61d219cd57ce80879f4951d7040ebeedb3230671
Mamta <mamtarajput1610@gmail.com>
2020-08-24 23:01:46 +0530

    Index and Common file added

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+function loadSignUpModal() {
+    document.getElementById("signinModal").style.display = "none";
+    document.getElementById("signupModal").style.display = "block";
+}
+function closeSignupModal() {
+    document.getElementById("signupModal").style.display = "none";
+}
+function loadSignInModal() {
+    document.getElementById("signinModal").style.display = "block";
+}
+function closeSigninModal() {
+    document.getElementById("signinModal").style.display = "none";
+}
+function loadCreatePostModal() {
+    document.getElementById("createpostModal").style.display = "block";
+}
+function closeCreatepostModal() {
+    document.getElementById("createpostModal").style.display = "none";
+}
